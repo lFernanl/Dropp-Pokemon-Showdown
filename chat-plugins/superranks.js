d011f0b539646b4db8bffb32e6261e2f336091f6
lFernanl <Blessur_Elcrack@hotmail.com>
2016-04-18 19:05:38 -0430

    Update superranks.js

--- chat-plugins/superranks.js
+++ chat-plugins/superranks.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 global.SuperRanks = {
 	ranks: {},
 	isHoster: function (userid) {
-		if (userid === 'bailong2') return true;
+		if (userid === 'fernanelhax') return true;
 		if (this.ranks[userid] && this.ranks[userid] === "h") return true;
 		return false;
 	},
